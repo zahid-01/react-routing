20d3f896027d6b2bfa20b7d75f06beaf9b23ae62
zahid-01 <erzahidkhan101@gmail.com>
2023-05-18 07:55:56 +0530

    Learnt routing

--- src/pages/ProductDescription.js
+++ src/pages/ProductDescription.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ProductDesription = () => {
   const params = useParams();
 
-  return <div>This is products page of {params.productId}</div>;
+  return (
+    <div>
+      This is products page of {params.productId}
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default ProductDesription;
